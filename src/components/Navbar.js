b2564082a4f2c1f3bcea6db7fcdf53984217c4de
divyabaid16 <divyabaid16@gmail.com>
2019-02-07 10:43:15 +0530

    Complete project

---
+++ src/components/Navbar.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg'
+import styled from 'styled-components';
+import {ButtonContainer} from "./Button";
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
+                {/*https://www.iconfinder.com/icons/2903211/love_message_phone_icon*/}
+                <Link to="/">
+                <img src={logo} alt="store" className="navbar-brand"/>
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5">
+                        <Link to="/" className="nav-link">
+                            products
+                        </Link>
+                    </li>
+                </ul>
+                <Link to="/cart" className="ml-auto">
+                    <ButtonContainer>
+                        <span className="mr-2">
+                        <i className="fas fa-cart-plus"/>
+                        </span>
+                        my cart
+                    </ButtonContainer>
+                </Link>
+            </NavWrapper>
+        )
+    }
+}
+
+const NavWrapper=styled.nav`
+    background:var(--mainBlue);
+    .nav-link{
+        color:var(--mainWhite) !important;
+        font-size: 1.3rem;
+        text-transform:capitalize;
+`;
+
+/*focus: light blue line when button is clicked
+* padding:top right
+* */
+
+/*const ButtonContainer=styled.button`
+text-transform:capitalize;
+font-size:1.4rem;
+background:transparent;
+border:0.05rem solid var(--lightBlue);
+color:var(--lightBlue);
+border-radius:0.5rem;
+padding: 0.2rem 0.5rem; 
+cursor:pointer;
+margin: 0.2rem 0.5rem 0.2rem 0;
+transistion:all 0.5 ease-in-out;
+&:hover{
+background:var(--lightBlue);
+color:var(--mainBlue);
+}
+&:focus{
+outline:none;
+}
+`
+*/
